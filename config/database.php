b373a10d0b99573c22520253bffb97e44e0a0a1d
MaximPozdnyakov <maximpozdnyakow@gmail.com>
2020-07-06 15:09:27 +0300

    v1.0.5

--- config/database.php
+++ config/database.php
@@ -32,6 +32,7 @@ return [
     | choice installed on your machine before you begin development.
     |
     */
+    $databaseUrl = parse_url(env('DATABASE_URL'));
 
     'connections' => [
 
@@ -64,13 +65,7 @@ return [
         ],
 
         'pgsql' => [
-            'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
